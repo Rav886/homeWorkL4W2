92f89abeda7af63bf35eba2a78b4dfab54265455
Ra v <timon521182@hotmail.com>
2023-02-12 13:27:29 +0100

    Add project files.

---
+++ homeWorkL4W2/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homeWorkL4W2
+{
+    public class Item
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+    }
+}
---
+++ homeWorkL4W2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace homeWorkL4W2
+{
+
+    class Program
+    {
+        public const string FILE_NAME = @"C:\Szkolenie\homrWorkL4W2.xlsx";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj imię pracownika");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Wprowadzone imię to: {name}");
+
+            Console.WriteLine("Podaj Nazwisko pracownika");
+
+            string surname = Console.ReadLine();
+
+            Console.WriteLine($"Wprowadzone nazwisko to: {surname}");
+
+            Console.WriteLine("Podaj wiek pracownika");
+
+            object Int = null;
+
+            int age = Console.ReadLine();
+            Int.TryParse(Console.ReadLine(), out age);
+            Console.WriteLine($"Wprowadzony wiek to: {age}");
+
+            Console.WriteLine("Podaj płeć pracownika \"m\" mężczyzna, \"k\" kobieta ");
+
+            string sex = Console.ReadLine();
+
+            Console.WriteLine($"Wprowadzona płeć to: {sex}");
+        }
+
+    }
+}
